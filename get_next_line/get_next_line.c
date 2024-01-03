f6b5dc5c212ad6b652ea469c64e9e0d45ba5a597
Torutu <120149333+Torutu@users.noreply.github.com>
2024-01-03 17:50:07 +0200

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: walnaimi <walnaimi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 13:03:33 by walnaimi          #+#    #+#             */
-/*   Updated: 2024/01/03 16:03:00 by walnaimi         ###   ########.fr       */
+/*   Updated: 2024/01/03 17:49:18 by walnaimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,38 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-
-#define BUFFER_SIZE 11
+#include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
-    static char buffer[BUFFER_SIZE];
-    int buff;
+    static char buffer[BUFFER_SIZE + 1];
+	char 		*newline_ptr;
+    int 		buff;
 
-    buff = read(fd, buffer, sizeof(buffer));
+    buff = read(fd, buffer, BUFFER_SIZE);
 
     if (buff <= 0)
     {
         buffer[0] = '\0';
         return buffer;
     }
-
-    // Find the position of the newline character (if any)
-    char *newline_ptr = strchr(buffer, '\n');
+    newline_ptr = strchr(buffer, '\n');
 
     if (newline_ptr != NULL)
     {
-    // If newline found, replace it with '\0' to terminate the string
         *newline_ptr = '\0';
         return buffer;
     }
 
-    // If no newline is found, return the entire buffer
     return buffer;
 }
 int main() {
     int fd;
 
     fd = open("text.txt", O_RDONLY);
-    if (fd == -1) {
-        perror("Error opening file");
-        return EXIT_FAILURE;
-    }
-
     char *line = get_next_line(fd);
     while (line[0] != '\0') {
-        printf("%s\n", line);
+        printf("%s.\n", line);
         line = get_next_line(fd);
     }
 
@@ -63,21 +54,3 @@ int main() {
 
     return 0;
 }
-/*int main()
-{
-	int fd;
-
-	fd = open("text.txt", O_RDONLY);
-	//char *line =  get_next_line(fd);
-	//while(line)
-	//{
-		//printf("%s", line);
-		//line = get_next_line(fd);
-	//}
-
-	printf("%s", get_next_line(fd));
-
-	close(fd);
-
-    return 0;
-}*/
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,25 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: walnaimi <walnaimi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/02 16:56:03 by walnaimi          #+#    #+#             */
+/*   Created: 2024/01/03 16:40:11 by walnaimi          #+#    #+#             */
 /*   Updated: 2024/01/03 17:49:20 by walnaimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <stdlib.h>
-# include <unistd.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2
-# endif
-
-char *get_next_line(int fd);
-
-#endif
+#include "get_next_line.h"
