2a43a3d52393ada21033fd253db5a5ae5aeac2ee
Torutu <120149333+Torutu@users.noreply.github.com>
2024-06-03 13:08:06 +0300

    Add files via upload

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,13 +6,13 @@
 /*   By: walnaimi <walnaimi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 18:03:22 by walnaimi          #+#    #+#             */
-/*   Updated: 2023/11/16 19:45:03 by walnaimi         ###   ########.fr       */
+/*   Updated: 2024/05/15 15:15:36 by walnaimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd)
+void	ft_putendl_fd(const char *s, int fd)
 {
 	int	i;
 
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: walnaimi <walnaimi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:58:54 by walnaimi          #+#    #+#             */
-/*   Updated: 2023/11/17 12:58:34 by walnaimi         ###   ########.fr       */
+/*   Updated: 2024/05/15 15:20:33 by walnaimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,17 +61,17 @@ char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
-void				ft_putendl_fd(char *s, int fd);
+void				ft_putendl_fd(const char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 /*******************************************************************/
 /*                            BONUS                                */
 /*******************************************************************/
 
 t_list				*ft_lstnew(void *content);
-void				ft_lstadd_front(t_list **lst, t_list *new);
+void				ft_lstadd_front(t_list **lst, t_list *new_lst);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
-void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstadd_back(t_list **lst, t_list *new_lst);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
